1c988549233804dacf4768a779872154f933428e
MaxBrych <max.brych03@gmail.com>
2024-05-26 03:04:18 +0200

    first commit

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,14 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract MyContract {
-    constructor() {}
+contract ThemeStorage {
+    mapping(address => string) private themeCIDs;
+
+    function setThemeCID(string memory cid) public {
+        themeCIDs[msg.sender] = cid;
+    }
+
+    function getThemeCID(address user) public view returns (string memory) {
+        return themeCIDs[user];
+    }
 }
